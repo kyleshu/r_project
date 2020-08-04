6fe8ee541cdf636b69dcc8d9a6da776e3649c541
Kyle Shu <shujunyi@gmail.com>
2020-08-04 18:35:46 +0800

    add forecasting days estimation

--- predict_csi300.R
+++ predict_csi300.R
@@ -4,7 +4,14 @@ library(timeSeries)
 library(forecast)
 library(xts)
 
-end_date <- "2020-08-01"
+fcast_start_date <- "2020-07-01"
+fcast_end_date <- "2020-08-01"
+
+# calculate forecasting days
+test_data <- getSymbols("603000.ss", auto.assign = FALSE, 
+                        from = fcast_start_date, to = fcast_end_date, 
+                        src = "yahoo")
+term <- nrow(test_data)
 
 # load CSI 300 stocks
 csi300 <- read.csv("data/csi300.csv", header = TRUE)
@@ -12,7 +19,7 @@ csi300 <- read.csv("data/csi300.csv", header = TRUE)
 # load CSI 300 stock price time series
 for (i in 1:nrow(csi300)) {
   data <- getSymbols(as.character(csi300[i,1]), auto.assign = FALSE, 
-                     from = "2015-01-01", to = end_date, src = "yahoo")
+                     from = "2015-01-01", to = fcast_start_date, src = "yahoo")
   abbr <- as.character(csi300[i,2])
   assign(abbr, data[,4])
 }
@@ -31,7 +38,6 @@ for (i in 1:nrow(csi300)) {
 }
 
 # use AIC to determine best model and parameters, and forecast 30-day yields
-term <- 30
 p_list <- 0:5
 q_list <- 0:5
 yields <- list()
