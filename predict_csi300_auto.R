73896a3d77db3c20b14eb84baa8b59ab3d0eab33
Kyle Shu <shujunyi@gmail.com>
2020-08-07 01:15:54 +0800

    add plots and logs

---
+++ predict_csi300_auto.R
@@ -0,0 +1,63 @@
+library(quantmod)
+library(tseries)
+library(timeSeries)
+library(forecast)
+library(xts)
+library(lubridate)
+
+startDate <- ymd('2019-09-01')
+endDate <- ymd('2019-10-01')
+startDates <- startDate %m+% months(c(0:10))
+endDates <- endDate %m+% months(c(0:10))
+
+for (m in 1:11) {
+
+  fcast_start_date <- as.character(startDates[m])
+  fcast_end_date <- as.character(endDates[m])
+  
+  # calculate forecasting days
+  test_data <- getSymbols("603000.ss", auto.assign = FALSE, 
+                          from = fcast_start_date, to = fcast_end_date, 
+                          src = "yahoo")
+  term <- nrow(test_data)
+  
+  # load CSI 300 stocks
+  csi300 <- read.csv("data/csi300.csv", header = TRUE)
+  
+  # load CSI 300 stock price time series
+  for (i in 1:nrow(csi300)) {
+    data <- getSymbols(as.character(csi300[i,1]), auto.assign = FALSE, 
+                       from = "2015-01-01", to = fcast_start_date, src = "yahoo")
+    abbr <- as.character(csi300[i,2])
+    assign(abbr, data[,4])
+  }
+  
+  # use auto ARIMA model, and forecast 30-day yields
+  df <- data.frame(Code=character(), 
+                   Yield=double(), 
+                   High=double(),
+                   stringsAsFactors=FALSE) 
+  for (i in 1:nrow(csi300)) {
+    # retrieve data
+    abbr <- as.character(csi300[i,2])
+    data <- get(abbr)
+    
+    # generate model
+    fit_model <- auto.arima(data, seasonal = FALSE)
+    
+    # forecast
+    fcast <- forecast(fit_model, h=term)
+    future_price <- fcast$mean[term]
+    max_price <- max(fcast$mean)
+    current_price <- as.double(data[length(data)])
+    yield <- (future_price/current_price)-1
+    high <- (max_price/current_price)-1
+    df2 <- data.frame(abbr, yield, high)
+    names(df2) <- c("Code", "Yield", "High")
+    df <- rbind(df, df2)
+  }
+  
+  # sort stocks and export
+  df <- df[order(df$Yield, decreasing = TRUE),]
+  write.csv(df, paste("result_auto/", fcast_start_date, ".csv", sep = ""))
+}
--- predict_stock.R
+++ predict_stock.R
@@ -4,35 +4,65 @@ library(timeSeries)
 library(forecast)
 library(xts)
 
-somestock <- getSymbols("^GSPC", auto.assign = FALSE, from = "2015-01-01", 
-           to = "2020-08-01", src = "yahoo")
+code <- "000300.ss"
+code2<- "SHSZ300"
+
+fn <- paste("log/", code2, ".txt", sep = "")
+sink(fn, append=FALSE, split=TRUE)
+
+somestock <- getSymbols(code, auto.assign = FALSE, 
+                        from = "2015-01-01", to = "2020-07-01", 
+                        src = "yahoo")
+test_data <- getSymbols(code, auto.assign = FALSE, 
+                        from = "2020-07-01", to = "2020-08-01", 
+                        src = "yahoo")
+term <- nrow(test_data)
+fn <- paste("plots/", code2, "_barchart.jpg", sep = "")
+jpeg(filename = fn)
 barChart(somestock)
+dev.off()
 
 somestock_close <- somestock[,4]
+fn <- paste("plots/", code2, "_close.jpg", sep = "")
+jpeg(filename = fn)
 plot(somestock_close)
+dev.off()
 
+fn <- paste("plots/", code2, "_acf+pacf.jpg", sep = "")
+jpeg(filename = fn)
 par(mfrow=c(1,2))
 Acf(somestock_close, main='ACF for Differenced Series')
 Pacf(somestock_close, main='PACF for Differenced Series')
+dev.off()
 
 print(adf.test(somestock_close))
 
-# 必要时进行差分
-# df <- ndiffs(somestock_close, test = "adf")
-# df_data <- diff(somestock_close, df)
-# somestock_close <- df_data[(df+1):nrow(df_data),]
-
 fitA <- auto.arima(somestock_close, seasonal = FALSE)
-tsdisplay(residuals(fitA), lag.max = 40, main='(5,1,5) Model Residuals')
+print(fitA)
+fn <- paste("plots/", code2, "_auto.jpg", sep = "")
+jpeg(filename = fn)
+tsdisplay(residuals(fitA), lag.max = 40, main='Auto Model Residuals')
+dev.off()
 
 fitB <- arima(somestock_close, order = c(1,1,1))
+fn <- paste("plots/", code2, "_111.jpg", sep = "")
+jpeg(filename = fn)
 tsdisplay(residuals(fitB), lag.max = 40, main='(1,1,1) Model Residuals')
+dev.off()
 
-term <- 100
+fn <- paste("plots/", code2, "_fcast_auto.jpg", sep = "")
+jpeg(filename = fn)
 fcast1 <- forecast(fitA, h=term)
 plot(fcast1)
+dev.off()
+
+fn <- paste("plots/", code2, "_fcast_111.jpg", sep = "")
+jpeg(filename = fn)
 fcast2 <- forecast(fitB, h=term)
 plot(fcast2)
+dev.off()
 
 accuracy(fcast1)
 accuracy(fcast2)
+
+sink()
